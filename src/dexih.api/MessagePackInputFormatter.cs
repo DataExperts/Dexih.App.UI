673f66504feff4532e689ba3ee945a7cc6c20fd3
garyholland <gholland@dataexpertsgroup.com>
2019-12-19 12:53:41 +1100

    messagepack issue with signalr

--- src/dexih.api/MessagePackInputFormatter.cs
+++ src/dexih.api/MessagePackInputFormatter.cs
@@ -1,30 +1,30 @@
-using System.Threading.Tasks;
-using MessagePack;
-using Microsoft.AspNetCore.Mvc.Formatters;
-
-namespace dexih.api
-{
-    public class MessagePackInputFormatter : IInputFormatter
-    {
-        private const string ContentType = "application/x-msgpack";
-        private readonly MessagePackSerializerOptions options;
-
-        public MessagePackInputFormatter(MessagePackSerializerOptions resolver)
-        {
-            this.options = resolver ?? MessagePackSerializer.DefaultOptions;
-        }
-
-        public bool CanRead(InputFormatterContext context)
-        {
-            return context.HttpContext.Request.ContentType == ContentType;
-        }
-
-        public Task<InputFormatterResult> ReadAsync(
-            InputFormatterContext context)
-        {
-            var request = context.HttpContext.Request;
-            return InputFormatterResult.SuccessAsync(
-                MessagePackSerializer.Deserialize(context.ModelType, request.Body, this.options));
-        }
-    }
-}
+// using System.Threading.Tasks;
+// using MessagePack;
+// using Microsoft.AspNetCore.Mvc.Formatters;
+//
+// namespace dexih.api
+// {
+//     public class MessagePackInputFormatter : IInputFormatter
+//     {
+//         private const string ContentType = "application/x-msgpack";
+//         private readonly MessagePackSerializerOptions options;
+//
+//         public MessagePackInputFormatter(MessagePackSerializerOptions resolver)
+//         {
+//             this.options = resolver ?? MessagePackSerializer.DefaultOptions;
+//         }
+//
+//         public bool CanRead(InputFormatterContext context)
+//         {
+//             return context.HttpContext.Request.ContentType == ContentType;
+//         }
+//
+//         public Task<InputFormatterResult> ReadAsync(
+//             InputFormatterContext context)
+//         {
+//             var request = context.HttpContext.Request;
+//             return InputFormatterResult.SuccessAsync(
+//                 MessagePackSerializer.Deserialize(context.ModelType, request.Body, this.options));
+//         }
+//     }
+// }
